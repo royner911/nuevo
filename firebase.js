aebb1ac2dc31b19c53760a672d17bfc0b9425f50
royner <royner454@gamil.com>
2025-04-05 14:55:26 -0600

    Migración completa a variables de entorno para Firebase

--- firebase.js
+++ firebase.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseConfig.json"); // Importa el JSON con credenciales
+require("dotenv").config(); // Cargar variables del archivo .env
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore(); // Conexión a Firestore
